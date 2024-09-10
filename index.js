5075a3602b3a9467b02ae623c0509481a0c12396
Srikar T <169169726+dis-craft@users.noreply.github.com>
2024-09-10 18:46:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ document.getElementById("submit").onclick = function(){
     CE_fuel = parseFloat(document.getElementById("fuel").value);
     CE_electricity = parseFloat(document.getElementById("electricity").value);
 
-    // Multiply factors for respective emissions
+
     let CE_coalprodMultiplied = CE_coalprod * 30;
     let CE_fuelMultiplied = CE_fuel * 230;
     let CE_electricityMultiplied = CE_electricity * 127;
 
-    // Display individual results
+
     document.getElementById("CE-output-coal-prod").textContent = `Carbon emission from coal production in one year is ${CE_coalprodMultiplied.toFixed(3)} KG`;
     document.getElementById("CE-output-vehicles").textContent = `Number of vehicles ${parseFloat(document.getElementById("no-of-vehicles").value)}`;
     document.getElementById("CE-output-fuel").textContent = `Carbon emission from fuel used in vehicles in one year is ${CE_fuelMultiplied.toFixed(3)} KG`;
     document.getElementById("CE-output-electricity").textContent = `Carbon emission from electricity used in one year is ${CE_electricityMultiplied.toFixed(3)} KG`;
 
-    // Calculate total carbon emissions and round it to 3 decimal places
+
     TCE = CE_coalprodMultiplied + CE_fuelMultiplied + CE_electricityMultiplied;
     document.getElementById("CE-output-total").textContent = `Total Carbon emission in one year is ${TCE.toFixed(3)} KG`;
 }
